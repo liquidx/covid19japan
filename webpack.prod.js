e4451684568a1f8ba87ae45f0da8bc00256066f5
Alastair Tse <alastair@liquidx.net>
2020-03-21 17:05:11 +0900

    Attempt to use tree-shaking (doesn't seem to work.)

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,6 +7,7 @@ const TerserPlugin = require('terser-webpack-plugin')
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
+    usedExports: true,
     minimize: true,
     minimizer: [new TerserPlugin()],
   },
