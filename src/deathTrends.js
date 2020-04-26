5ce883aab2cf114d64814b250a5aa75738976818
Alastair Tse <alastair@liquidx.net>
2020-04-26 22:25:26 +0900

    Update deathtrends graph

--- src/deathTrends.js
+++ src/deathTrends.js
@@ -1,6 +1,8 @@
 import _ from "lodash";
 import c3 from "c3";
 
+import { COLOR_DECEASED } from "./data/constants.js";
+
 const generateDeathDataset = (patients) => {
   let deathsByDay = {};
   for (let patient of patients) {
@@ -13,6 +15,7 @@ const generateDeathDataset = (patients) => {
       deathsByDay[dateAnnounced] = {
         patients: [],
         deaths: 0,
+        deathsAgeUnknown: 0,
         deathsOver60: 0,
         deathsUnder60: 0,
       };
@@ -21,7 +24,9 @@ const generateDeathDataset = (patients) => {
     if (patient.patientStatus == "Deceased") {
       deathsByDay[dateAnnounced].deaths += 1;
       deathsByDay[dateAnnounced].patients.push(patient);
-      if (patient.ageBracket != -1) {
+      if (patient.ageBracket == -1) {
+        deathsByDay[dateAnnounced].deathsAgeUnknown += 1;
+      } else {
         if (patient.ageBracket >= 60) {
           deathsByDay[dateAnnounced].deathsOver60 += 1;
         } else {
@@ -37,6 +42,7 @@ const generateDeathDataset = (patients) => {
         date: v[0],
         patients: v[1].patients,
         deaths: v[1].deaths,
+        deathsAgeUnknown: v[1].deathsAgeUnknown,
         deathsUnder60: v[1].deathsUnder60,
         deathsOver60: v[1].deathsOver60,
       };
@@ -45,16 +51,26 @@ const generateDeathDataset = (patients) => {
   );
 };
 
+const movingAverage = (values, period) => {
+  let averages = [];
+  _.forEach(values, (v, i) => {
+    let extent = _.slice(values, Math.max(0, i - period), i + 1);
+    averages.push(_.sum(extent) / extent.length);
+  });
+  return averages;
+};
+
 const generateChartData = (deaths) => {
-  let xAxisData = ["x"];
-  let deathsData = ["deaths"];
+  let xAxisData = ["date"];
+  let deathsData = ["allDeaths"];
+  let deathsAgeUnknownData = ["deathsAgeUnknown"];
   let deathsOver60Data = ["deathsOver60"];
   let deathsUnder60Data = ["deathsUnder60"];
   _.forEach(deaths, (v) => {
     xAxisData.push(v.date);
   });
-  _.forEach(deaths, (v) => {
-    deathsData.push(v.deaths);
+  _.forEach(movingAverage(_.map(deaths, "deaths"), 7), (v) => {
+    deathsData.push(v);
   });
   _.forEach(deaths, (v) => {
     deathsOver60Data.push(v.deathsOver60);
@@ -62,14 +78,21 @@ const generateChartData = (deaths) => {
   _.forEach(deaths, (v) => {
     deathsUnder60Data.push(v.deathsUnder60);
   });
-  return [xAxisData, deathsData, deathsOver60Data, deathsUnder60Data];
+  _.forEach(deaths, (v) => {
+    deathsAgeUnknownData.push(v.deathsAgeUnknown);
+  });
+  return [
+    xAxisData,
+    deathsData,
+    deathsAgeUnknownData,
+    deathsUnder60Data,
+    deathsOver60Data,
+  ];
 };
 
 let _deathsChart = null;
 const renderDeaths = (deaths) => {
   let deathsChartData = generateChartData(_.slice(deaths, 40, -2));
-  console.log(deathsChartData);
-
   let chartElement = document.querySelector("#death-trend .chart");
   _deathsChart = c3.generate({
     bindto: chartElement,
@@ -78,13 +101,24 @@ const renderDeaths = (deaths) => {
     tooltip: { show: false },
     legend: { show: true },
     point: { show: false },
+    x: "date",
     data: {
-      type: "bar",
-      x: "x",
+      x: "date",
       columns: deathsChartData,
+      groups: [["deathsAgeUnknown", "deathsUnder60", "deathsOver60"]],
+      colors: {
+        allDeaths: COLOR_DECEASED,
+        deathsAgeUnknown: "rgb(220, 200, 200)",
+        deathsUnder60: "rgb(220, 160, 160)",
+        deathsOver60: "rgb(220, 120, 120)",
+      },
+      type: "bar",
+      types: {
+        allDeaths: "spline",
+      },
     },
     bar: {
-      width: { ratio: 1.0 },
+      width: { ratio: 0.8 },
     },
     axis: {
       x: {
@@ -96,7 +130,7 @@ const renderDeaths = (deaths) => {
 };
 
 const main = () => {
-  fetch("https://data.covid19japan.com/patient_data/latest.json")
+  fetch("http://localhost:3999/patient_data/latest.json")
     .then((response) => response.json())
     .then((patients) => {
       let deathDataset = generateDeathDataset(patients);
